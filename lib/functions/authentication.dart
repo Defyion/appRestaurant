c18420e71a1447050a9783a4273c75d30dbcdb65
Guilherme Soares <gui_coimbra4@hotmail.com>
2020-05-14 18:35:46 -0400

    corrigindo algumas coisas

--- lib/functions/authentication.dart
+++ lib/functions/authentication.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-var email = 1234;
-var password = 1234;
 
 class Authenticator extends StatefulWidget {
-    final String email;
-    final String password;
-    Authenticator({@required this.email, this.password}); _AuthenticatorState createState() => _AuthenticatorState();
-
+    final AdmLogin;
+    Authenticator({@required this.AdmLogin});
+    _AuthenticatorState createState() => _AuthenticatorState();
   }
   
 class _AuthenticatorState extends State<Authenticator> {
@@ -19,5 +16,3 @@ class _AuthenticatorState extends State<Authenticator> {
       );
     }
   }
-
-
