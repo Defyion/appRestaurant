db193523f71e548b9fce7287f2c6a63f914fa783
Guilherme Soares <gui_coimbra4@hotmail.com>
2020-05-16 11:47:32 -0400

    Add documents

--- lib/functions/add.dart
+++ lib/functions/add.dart
@@ -2,13 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Add {
   void addFood(String image, String title, String description, double price) {
-    Firestore.instance.collection('produtos').document().setData({
+    Firestore.instance.collection('produtos').document('foods').setData({
       image: image,
       title: title,
       description: description,
     });
   void addDrink(String image, String title, String description, double price) {}
-    Firestore.instance.collection('produtos').document().setData({
+    Firestore.instance.collection('produtos').document('drinks').setData({
       image: image,
       title: title,
       description: description,
